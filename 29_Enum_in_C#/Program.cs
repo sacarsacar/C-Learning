b0388c4242b61353767b10ac6bf970e1c90b15fe
sacarsacar <104842066+sacarsacar@users.noreply.github.com>
2024-07-24 08:40:01 +0545

    Add files via upload

---
+++ 29_Enum_in_C#/Program.cs
@@ -0,0 +1,17 @@
+﻿// enums 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Days.Sunday + " is a Day.");  // printing String
+        Console.WriteLine((int)Days.Monday + " = Monday");   // Printing Int Along with string
+
+        Console.ReadKey();
+    }
+}
+
+// Use enums when you know values you have won't change.
+enum Days
+{
+    Sunday = 1, Monday = 2, Tuesday = 3, Wednesday = 4,Thursday = 5, Friday = 6, Saturday = 7
+}
