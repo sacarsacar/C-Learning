7e75f062d3541323b50941209ad7c87d005aad9c
sacarsacar <104842066+sacarsacar@users.noreply.github.com>
2024-07-24 08:36:09 +0545

    Add files via upload

---
+++ 25_Polymorphism_in_C#/Program.cs
@@ -0,0 +1,55 @@
+﻿// polymorphism: Having many forms !  (Here: Vehicle is having many forms.)
+using System.ComponentModel.DataAnnotations;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+        Bike bike = new Bike();
+
+        // Declearing array
+        Vehicle[] vehicles = { car, bicycle, bike };
+
+        // Using foreach loop to display:
+        foreach(Vehicle vehicle in vehicles)
+        {
+            vehicle.Start();
+        }
+
+        Console.ReadKey();
+    }
+}
+
+// Creating Vehicle Class
+class Vehicle
+{
+    public virtual void Start() { }
+}
+
+class Car : Vehicle
+{
+    // Method Overriding
+    public override void Start() {
+        Console.WriteLine("The Car is Racing!");
+    }
+}
+
+class Bicycle : Vehicle
+{
+    public override void Start()
+    {
+        Console.WriteLine("The Bicycle is Racing!");
+    }
+}
+
+class Bike : Vehicle
+{
+    public override void Start()
+    {
+        Console.WriteLine("The Bike is Racing!");
+    }
+}
+
+
