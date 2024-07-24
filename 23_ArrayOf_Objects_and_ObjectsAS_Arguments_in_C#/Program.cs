96ea38719263352979ca57a32a0995d4f0ecefd7
sacarsacar <104842066+sacarsacar@users.noreply.github.com>
2024-07-24 08:34:31 +0545

    Add files via upload

---
+++ 23_ArrayOf_Objects_and_ObjectsAS_Arguments_in_C#/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Arrays in objects:
+
+        car[] collection = { new car("BYD"), new car("Tesla"), new car("Kia EV6") };
+
+
+        Console.WriteLine("-----Collections of Cars:----\t");
+
+        foreach (car Car in collection)
+        {
+            Console.WriteLine("Name: " + Car.model);
+        }
+
+        Console.ReadKey();
+    }
+    class car
+    {
+        public string model;
+
+        public car(string model)
+        {
+            this.model = model;
+        }
+
+    }
+    
+}
