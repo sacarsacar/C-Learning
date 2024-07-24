1f9b463fd8dca1d4818e8fc80fcc02eabe91e1c7
sacarsacar <104842066+sacarsacar@users.noreply.github.com>
2024-07-24 08:22:11 +0545

    Add files via upload

---
+++ 10_Arrays_in_C#/Program.cs
@@ -0,0 +1,34 @@
+﻿// Arrays:
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] names = { "Sakar", "Anup", "Santosh", "Manjil", "Utshav", "Surendra" };
+
+        // Displaying one at a time:
+        Console.WriteLine(names[0]);
+        Console.WriteLine("\n-------------------------------------------\n");
+        // Changing item of array:
+        names[5] = "Sujan";
+
+        // Displaying all items using for loop:
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+
+        Console.WriteLine("\n-------------------------------------------\n");
+
+
+        // using for each loop:
+        foreach(string name in names)
+        {
+            Console.WriteLine(name);     
+        }
+
+
+
+
+        Console.ReadKey();
+    }
+}
