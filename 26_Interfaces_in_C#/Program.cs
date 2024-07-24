604e9d28240418268ef6bc5fae97e1e6d43ce5d6
sacarsacar <104842066+sacarsacar@users.noreply.github.com>
2024-07-24 08:37:10 +0545

    Add files via upload

---
+++ 26_Interfaces_in_C#/Program.cs
@@ -0,0 +1,60 @@
+﻿// interfaces: Defines a "Contract" that all the classes inheriting from should follow: An interface declares " what a class should have" 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Rabbit rabbit = new Rabbit();
+        Hawk hawk = new Hawk();
+        Fish fish = new Fish();
+
+        rabbit.Flee();
+        hawk.Hunt();
+        fish.Flee();
+        fish.Hunt();
+
+        Console.ReadKey();
+    }
+
+    // should use I while creating Interfaces:
+    interface IPrey
+    {
+        void Flee();
+    }
+
+    // Predator interface 
+    interface IPredator
+    {
+        void Hunt();  // Method Hunt
+    }
+
+    class Rabbit : IPrey
+    {
+        public void Flee()
+        {
+            Console.WriteLine("The Rabbit runs away!");
+        }
+    }
+
+    class Hawk : IPredator
+    {
+        public void Hunt()
+        {
+            Console.WriteLine("The Hawk is Searching For Food!");
+        }
+    }
+
+    //Method Using Both Interfaces:
+    class Fish : IPrey,IPredator
+    {
+
+        public void Flee()
+        {
+            Console.WriteLine("The Fish is Swimming!");
+        }
+        public void Hunt()
+        {
+            Console.WriteLine("The Fish is Searching For Food!");
+        }
+    }
+
+}
