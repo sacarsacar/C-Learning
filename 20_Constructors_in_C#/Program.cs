f3c40e305ffdf4509283d93263fb9b1c9898b2ec
sacarsacar <104842066+sacarsacar@users.noreply.github.com>
2024-07-24 08:32:02 +0545

    Add files via upload

---
+++ 20_Constructors_in_C#/Program.cs
@@ -0,0 +1,34 @@
+﻿// Constructures:
+internal class Program
+
+{
+    static void Main(string[] args)
+    {
+        Car car1 = new Car("Ford", "Mustang",2001,"red");
+
+        car1.Drive();
+
+        Console.ReadKey();
+    }
+    
+    class Car
+    {
+        string make;
+        string model;
+        int year;
+        string color;
+
+        public Car(string make, string model, int year, string color)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine($"You are driving the {make} {model} ");
+        }
+    }
+}
