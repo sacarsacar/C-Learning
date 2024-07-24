6683eeaee699ea36076be9fe4fb59fd6e674dc13
sacarsacar <104842066+sacarsacar@users.noreply.github.com>
2024-07-24 08:26:27 +0545

    Add files via upload

---
+++ 14_Methods_in_C#/Program.cs
@@ -0,0 +1,27 @@
+﻿// methods:
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 0; 
+        string name = "";
+
+        HappyBirthday(age = 10, name ="Sakar Chaulagain");   // Calling method
+
+        HappyBirthday(age = 18, name = "Zoombie");
+
+        Console.ReadKey();
+    }
+
+    // method
+    static void HappyBirthday (int age, string name){     
+
+        Console.WriteLine("Happy Birthday To You !");
+        Console.WriteLine("Happy Birthday Dear " + name);
+        Console.WriteLine($"You Are Now {age} years old");
+        Console.WriteLine("Happy Birthday To You !");
+        Console.WriteLine();
+        
+    }
+    
+}
