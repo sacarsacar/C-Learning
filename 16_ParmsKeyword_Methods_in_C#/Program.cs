91a7a7ab95f1155a13160252d23001ee67c56dee
sacarsacar <104842066+sacarsacar@users.noreply.github.com>
2024-07-24 08:28:13 +0545

    Add files via upload

---
+++ 16_ParmsKeyword_Methods_in_C#/Program.cs
@@ -0,0 +1,29 @@
+﻿// Params Keyword:
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        // MUltiple inputs:
+        double total = Bills(10, 20, 30, 30.5);
+
+        Console.WriteLine($"Total price is : {total}");
+
+        Console.ReadKey();
+    }
+
+    // params = can take multile values as a time(one dimensinal array)
+
+    static double Bills(params double[] prices)
+    {
+        double total = 0;
+
+        // using foreach loop for calculation
+        foreach(double price in prices)
+        {
+            total += price;
+        }
+
+        return total;
+    }
+}
