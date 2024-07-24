cc8202f84acde68b2474e8c37182c4976e242936
sacarsacar <104842066+sacarsacar@users.noreply.github.com>
2024-07-24 08:23:05 +0545

    Add files via upload

---
+++ 10_Logical_Operators_in_C#/Program.cs
@@ -0,0 +1,28 @@
+﻿// Logical operator:
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Your Age: ");
+        double Age = Convert.ToDouble(Console.ReadLine());
+
+
+        // Both the Conditions must meet.
+        if ( Age >= 18 &&  Age <= 24)
+        {
+            Console.WriteLine($" Your  Age is {Age} Years Old. \t \n You are Consider as Adult.");
+        }
+
+        // Only one condition must meet.
+        else if (Age < 18 ||  Age > 24)
+        {
+            Console.WriteLine("Your Age is " + Age + " years old. \t \n You are not Considerd as Adult." );
+        }
+
+        else {
+            Console.WriteLine(" Please Enter a valid Age!");
+        }
+
+        Console.ReadKey();
+    }
+}
