d4ec0a414ac6b7f336946a315d6e25aab161542b
sacarsacar <104842066+sacarsacar@users.noreply.github.com>
2024-07-24 08:29:03 +0545

    Add files via upload

---
+++ 17_ExceptionHandling_in C#/Program.cs
@@ -0,0 +1,41 @@
+﻿//Exception Handling:
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        double a;
+        double b;
+        double result;
+
+        // Write code which may cause error in try block:
+        try
+        {
+            Console.Write("Enter num 1:");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter num 2:");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            result = a / b;
+            Console.WriteLine("Result: " + result);
+        }
+
+        // Display error message:
+        catch(FormatException) {
+            Console.WriteLine("PLEASE Enter Number only!");
+        }
+
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Can't Divide by Zero!");
+        }
+
+        // Always execute
+        finally
+        {
+            Console.WriteLine("\t\t\t\t\nHAVE A GOOD DAY!");
+        }
+       
+        Console.ReadKey();
+    }
+}
