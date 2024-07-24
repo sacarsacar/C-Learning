9b26f600349d38fef5085ea5f6fa9b5df055e952
sacarsacar <104842066+sacarsacar@users.noreply.github.com>
2024-07-24 08:27:25 +0545

    Add files via upload

---
+++ 15_MethOverloading_Return_in_C#/Program.cs
@@ -0,0 +1,38 @@
+﻿// Return keyword and Method Overloading:
+internal class Program
+{
+  static void Main(string[] args)
+    {
+        double a;
+        double b;
+        double result;
+
+        Console.WriteLine("Multiplication:");
+
+        Console.Write("Enter num 1: ");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter num 2: ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        result = Multiply(a, b);
+
+        Console.WriteLine($"The multiplication of {a} * {b} = {result}");
+       
+
+        Console.ReadKey();
+    }
+
+    // use of return:
+    static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    // Method Overloading: (Assigning new variable or value to existing method )
+
+   // static double Multiply(double a, double b,double c)
+   // {
+     //   return a * b * c;
+    //}
+}
